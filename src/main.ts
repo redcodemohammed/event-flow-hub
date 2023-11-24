6c68ca98512d27a816b8cad2239dba96670fdeb6
redcodemohammed <redcodemohammed@gmail.com>
2023-11-24 22:19:33 +0300

    feat: refactor main.ts - added versioning - added global prefix - enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // http://locahost:3333/v1
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  // http://locahost:3333/api/v1
+  app.setGlobalPrefix('api');
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
